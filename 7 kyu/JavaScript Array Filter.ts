4c985459c4e503520bc805b294abfae2a93c66cf
Denis Zomarev <eek@live.ru>
2024-10-07 21:54:56 +0300

    new files: 7 kyu/JavaScript Array Filter.ts, 7 kyu/Sort Numbers.ts

---
+++ 7 kyu/JavaScript Array Filter.ts
@@ -0,0 +1,9 @@
+/* 
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+export const getEvenNumbers = (numbersArray: number[]): number[] => {
+  return numbersArray.filter((el) => el % 2 == 0);
+};
