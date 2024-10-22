5c750ee2f8dc34d87303074cdb08a251d2f02b5f
Denis Zomarev <eek@live.ru>
2024-10-22 23:18:24 +0300

    modified: 8 kyu/L1 Bartender, drinks.ts

--- 8 kyu/L1 Bartender, drinks.ts
+++ 8 kyu/L1 Bartender, drinks.ts
@@ -14,3 +14,23 @@ Note: anything else is the default case: if the input to the function is not any
 Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
 */
 
+function getDrinkByProfession(param: string): string {
+  param = param.toLowerCase();
+
+  switch (param) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
+}
